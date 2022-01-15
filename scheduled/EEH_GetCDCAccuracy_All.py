7ab16746d2ac376607b9cbf1aba9c008b768ed8c
Ethan Hunt <chessking94@gmail.com>
2022-01-15 13:38:07 -0600

    Renamed CDC Accuracy file

--- scheduled/EEH_GetCDCAccuracy_All.py
+++ scheduled/EEH_GetCDCAccuracy_All.py
@@ -6,8 +6,6 @@ import json
 def main():
     yrs = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
     mths = ['01', '02', '03', '04', '05' , '06', '07', '08', '09', '10', '11', '12']
-    #yrs = ['2021']
-    #mths = ['12']
 
     today = dt.date.today()
     first = today.replace(day=1)
@@ -28,6 +26,7 @@ def main():
         imp_write.write('SourceID' + DELIM + 'WhiteAccuracy' + DELIM + 'BlackAccuracy' + NEWLINE)
         for yr in yrs:
             for mth in mths:
+                print('Currently reviewing games from ' + yr + mth)
                 url = 'https://api.chess.com/pub/player/' + usr + '/games/' + yr + '/' + mth
                 resp = requests.get(url)
                 if resp.status_code == 404:
